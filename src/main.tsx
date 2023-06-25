98f08b97c5b1b9c6c493403863260d260556ffbe
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2023-06-25 18:16:47 +0900

    add introduce

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Introduce } from './pages/Introduce.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Introduce />
+	</React.StrictMode>,
 )
